36267d2508953e92e7c9f58dc0e8560e18b81f2a
Elise Erickson <elisece717@gmail.com>
2023-12-31 09:32:25 -0500

    adds semantic ui and basic header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import icon from './assets/icon.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img class="ui medium circular image" src={icon} alt="Call Pad Logo"/>
+        <h1 className="ui header">Call Pad Systems</h1>
+        <h3 className ="ui header">custom patient arrival and clinic solutions</h3>
       </header>
     </div>
   );
