05d24614822adb61ba6c718c06595133318109eb
Elise Erickson <elisece717@gmail.com>
2024-01-02 10:41:11 -0500

    moves app details to custom component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import icon from './assets/icon.png'
 import {Container} from 'semantic-ui-react'
+import ContactForm from './ContactForm';
+import AppInfoRow from './AppInfoRow';
 
 function App() {
   return (
@@ -16,19 +18,20 @@ function App() {
         
       <Container>
         <h2 className="ui header">The App</h2>
-        <h3 className="ui header">Patient Arrival Notifications</h3>
-        <p> Send push notifications directly to employees' smart phones and watches whenever a patient arrives with a single tap.</p>
-        <h3 className="ui header">Custom Messages</h3>
-        <p>Send customized messages with each notification to communicate patient information, appointment information, and more.</p>
-        <h3 className="ui header">Simple, Clean Interfaces</h3>
-        <div style={{flexDirection: 'row', display: 'flex'}}>
-          <p>The front desk dashboard displays all employees on a single color-coded screen.</p>
-          <p>Each employee's dashboard includes only the important information, nothing more.</p>
-        </div>
-
-        
-        
+        <AppInfoRow
+        title="Patient Arrival Notifications"
+        body="Send push notifications directly to employees' smart phones and watches whenever a patient arrives with a single tap."
+         />
+         <AppInfoRow
+         title="Custom Messages" 
+         body="Send customized messages with each notification to communicate patient information, appointment information, and more."/>
+         <AppInfoRow 
+         title="Simple, Clean Interfaces"
+         body="Each employee's dashboard includes only the important information, nothing more."
+         />
       </Container>
+
+      <ContactForm/>
     </div>
   );
 }
---
+++ src/ContactForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ContactForm(){
+    return(
+        <form>
+            <h2 className="ui header">Contact us to set up your own system.</h2>
+        </form>
+    )
+}
